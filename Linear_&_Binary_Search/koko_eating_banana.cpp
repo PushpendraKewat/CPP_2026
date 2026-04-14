8d298707c9187048ac4e40dcac92fad4eca39adb
PushpendraKewat <pushpendrakewat9@gmail.com>
2026-04-14 07:37:07 +0530

    14 april 2026

---
+++ Linear_&_Binary_Search/koko_eating_banana.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+
+int minEatingSpeed(vector<int>& piles, int h) {
+    int low = 1;
+    int high = *max_element(piles.begin(), piles.end());
+
+    while(low <= high){
+        int mid = low + (high - low) / 2;
+
+        long long hours = 0;
+
+        for(int i = 0; i < piles.size(); i++){
+            hours += (piles[i] + mid - 1) / mid; // ceil division
+        }
+
+        if(hours <= h){
+            high = mid - 1;
+        }
+        else{
+            low = mid + 1;
+        }
+    }
+    return low;
+}
+
+int main(){
+    vector<int> piles = {3,6,7,11};
+    int h = 8;
+
+    int result = minEatingSpeed(piles, h);
+
+    cout << "Minimum eating speed: " << result << endl;
+
+    return 0;
+}
