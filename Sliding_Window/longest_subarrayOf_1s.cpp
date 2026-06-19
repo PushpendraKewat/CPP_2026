0e395b7e45e293ad13589ff67e0ce549f149e581
PushpendraKewat <pushpendrakewat9@gmail.com>
2026-06-19 09:40:15 +0530

    19 june 2026

---
+++ Sliding_Window/longest_subarrayOf_1s.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+
+using std::cout;
+using std::endl;
+using std::vector;
+
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0;
+        int ans = 0;
+        int zero = 0;
+
+        for (int j = 0; j < n; j++) {
+            if (nums[j] == 0) {
+                zero++;
+            }
+
+            while (zero > 1) {
+                if (nums[i] == 0) {
+                    zero--;
+                }
+                i++;
+            }
+
+            ans = std::max(ans, j - i);
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<int> nums = {1, 1, 0, 1};
+
+    cout << "Longest Subarray Length: "
+         << sol.longestSubarray(nums) << endl;
+
+    return 0;
+}
