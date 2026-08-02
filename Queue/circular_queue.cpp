423099b40ad82a2a6d17057c73c6bdb4e9fdea9a
PushpendraKewat <pushpendrakewat9@gmail.com>
2026-08-02 07:31:42 +0530

    2 auguest 2026

--- Queue/circular_queue.cpp
+++ Queue/circular_queue.cpp
@@ -1,50 +1,49 @@
 #include <iostream>
 
-using std::cout;
-using std::endl;
+using namespace std;
 
-class Queue {
+class CircularQueue {
 public:
     int f;
     int b;
     int s;
     int arr[5];
 
-    Queue() {
+    CircularQueue() {
         f = 0;
         b = 0;
         s = 0;
     }
 
     void push(int val) {
-        if (b == 5) {
+        if (s == 5) {
             cout << "Queue is full\n";
             return;
         }
 
         arr[b] = val;
-        b++;
+        b = (b + 1) % 5;
         s++;
     }
 
     void pop() {
-        if (f == b) {
+        if (s == 0) {
             cout << "Queue is empty\n";
             return;
         }
 
-        f++;
+        f = (f + 1) % 5;
         s--;
     }
 
     int Front() {
-        if (size() == 0) return -1;
+        if (s == 0) return -1;
         return arr[f];
     }
 
     int Back() {
-        if (size() == 0) return -1;
-        return arr[b - 1];
+        if (s == 0) return -1;
+        return arr[(b - 1 + 5) % 5];
     }
 
     int size() {
@@ -52,32 +51,47 @@ public:
     }
 
     bool empty() {
-        return f == b;
+        return s == 0;
     }
 
     void display() {
-        for (int i = f; i < b; i++) {
+        if (s == 0) {
+            cout << "Queue is empty\n";
+            return;
+        }
+
+        int i = f;
+        for (int cnt = 0; cnt < s; cnt++) {
             cout << arr[i] << " ";
+            i = (i + 1) % 5;
         }
         cout << endl;
     }
 };
 
 int main() {
-    Queue q;
+    CircularQueue q;
 
     q.push(10);
     q.push(20);
     q.push(30);
     q.push(40);
     q.push(50);
-    q.push(60);   // Queue is full
 
     q.display();
-
-    cout << q.Front() << endl;
-    cout << q.Back() << endl;
 
     q.pop();
+    q.pop();
+
     q.display();
+
+    q.push(60);
+    q.push(70);
+
+    q.display();
+
+    cout << "Front = " << q.Front() << endl;
+    cout << "Back = " << q.Back() << endl;
+
+    return 0;
 }
